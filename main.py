d46fbae049abe1dfa0d22f4fe46d861abc4ac7a3
codebyThiagoA <thiago.walvesb@gmail.com>
2025-03-31 16:36:17 -0300

    correção do bug executavel pygame

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ screen = pg.display.set_mode((800, 600)) #Cria a janela do jogo
 clock = pg.time.Clock() #Inicia um relógio no jogo
 
 #goleiro
-screen_width = screen.get_width #Recebe a largura da tela
+screen_width = screen.get_width() #Recebe a largura da tela
 goalkeeper = Goalkeeper(screen_width // 2 - 50, 500, 5, 100) #Posição inicial do goleiro no centro
 
 running = True
@@ -19,13 +19,13 @@ while running: #loop principal
 
             running = False #Para o looping do jogo se o jogador fechar
 
-    keys = pg.key.get_pressed #Armazena as teclas pressionadas pelo jogador
+    keys = pg.key.get_pressed() #Armazena as teclas pressionadas pelo jogador
     goalkeeper.move(keys, screen_width) #Baseado nas teclas pressionadas, desloca o jogador pra esquerda ou pra direita
 
     screen.fill((0, 128, 0))  #Background verde
     pg.draw.rect(screen, (0, 0, 255), (goalkeeper.x, goalkeeper.y, goalkeeper.width, 50))  #Desenha um goleiro provisorio azul
 
     pg.display.update() #Atualiza na tela as posições dos objtos
-    clock.tick(30)#FPS do game
+    clock.tick(80)#FPS do game
 
 pg.quit()
