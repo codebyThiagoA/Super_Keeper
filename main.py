0852aa4943e972e3e57e859a36346ec68e739ad7
HugoCrasto07 <hugokcrasto@gmail.com>
2025-04-01 15:21:28 -0300

    fiz um pouco do looprincipal e fiz as colisões

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-eu=3
+import pygame as pg
+from sprite_sheet import sprites_player
+
+
+
+
+
+
+
+
+
+
+
+
+esc=False
+
+while running:
+
+        screen.blit(background_game, (0,0))
+        
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                running = False
+                exit()
+        
+
+
+        if not esc:
+             
+
+            removidos = []
+            colididos = []
+            for item in onscreen:
+                colisoes = pygame.sprite.spritecollide(goleiro, grupo_bolas, True)
+                pontuacao += len(colisoes)
