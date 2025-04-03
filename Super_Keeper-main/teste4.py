431ac12bc8b830f5c629509351b88e59dd0a43d4
PedroCavalcanti2 <pcavalacnti77@gmail.com>
2025-04-03 18:56:59 -0300

    ajuste largura x altura

--- Super_Keeper-main/teste4.py
+++ Super_Keeper-main/teste4.py
@@ -4,7 +4,7 @@ import random
 
 pygame.init()
 
-altura, largura = 800, 700
+altura, largura = 700, 800
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Sistema de Bolas")
 
