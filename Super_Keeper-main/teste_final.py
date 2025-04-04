60ab9490905d5f3a0b3c804be853542a5f59013f
PedroCavalcanti2 <pcavalacnti77@gmail.com>
2025-04-03 21:35:32 -0300

    mudanças gerais

--- Super_Keeper-main/teste_final.py
+++ Super_Keeper-main/teste_final.py
@@ -4,7 +4,7 @@ import random
 
 pygame.init()
 
-altura, largura = 800, 600
+altura, largura = 800, 700
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Sistema de Bolas")
 
@@ -19,14 +19,8 @@ class BolaBase:
         self.timer = 0
         self.bolas = []
 
-        # Carrega imagem ou cria uma bola colorida se não encontrar
-        try:
-            self.img = pygame.image.load(imagem_path).convert_alpha()
-            self.img = pygame.transform.scale(self.img, (60, 60))
-        except:
-            self.img = pygame.Surface((60, 60), pygame.SRCALPHA)
-            cor = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
-            pygame.draw.circle(self.img, cor, (30, 30), 30)
+        self.img = pygame.image.load(imagem_path).convert_alpha()
+        self.img = pygame.transform.scale(self.img, (60, 60)) 
 
     def velocidade_atual(self, pontos):
         return self.v_base + (self.v_base * pontos / 150)
@@ -72,28 +66,28 @@ class BolaBase:
 class Bola1(BolaBase):
     def __init__(self):
         super().__init__(
-            velocidade_base=2, 
+            velocidade_base=1.25, 
             dano=1, 
-            intervalo_base=180,  # 60 FPS * 3 segundos = 180 frames
-            imagem_path="bola1.png"
+            intervalo_base=254,  # 60 FPS * 3 segundos = 180 frames
+            imagem_path="Bola1.png"
         )
 
 class Bola2(BolaBase):
     def __init__(self):
         super().__init__(
-            velocidade_base=2,4, 
+            velocidade_base=(2.75), 
             dano=2, 
-            intervalo_base=300,  # 60 FPS * 5 segundos = 300 frames
-            imagem_path="bola2.png"
+            intervalo_base=567,  # 60 FPS * 7.5 segundos = 450 frames
+            imagem_path="Bola2.png"
         )
 
 class Bola3(BolaBase):
     def __init__(self):
         super().__init__(
-            velocidade_base=3, 
+            velocidade_base=3.84, 
             dano=3, 
-            intervalo_base=420,  # 60 FPS * 7 segundos = 420 frames
-            imagem_path="bola3.png"
+            intervalo_base=823,  # 60 FPS * 10 segundos = 600 frames
+            imagem_path="Bola3.png"
         )
 # Instanciar bolas
 bola1 = Bola1()
