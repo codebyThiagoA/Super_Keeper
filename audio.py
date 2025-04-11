4cd4337a5964a2ff4b4c33e56b0baf20d380ca36
PedroCavalcanti2 <pcavalacnti77@gmail.com>
2025-04-11 09:17:13 -0300

    teste

--- audio.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-som_tela_inicial = pygame.mixer.Sound('audio/som_tela_inicial.wav')
-som_estadio = 'audio/som_estadio.mp3'
-som_perda_vida = pygame.mixer.Sound('audio/som_perda_vida.wav')
-som_defesa = pygame.mixer.Sound('audio/som_defesa.wav')
-som_derrota = pygame.mixer.Sound('audio/som_derrota.wav')
